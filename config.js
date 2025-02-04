e22b984b1999cb05fb5527df26a40901e34f8e9e
goldenqueendatabase <goldenqueendatabase@gmail.com>
2025-02-04 22:06:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   //  සෙසන් අයිඩී එක දාන්න👇👇👇 (උඩු කොමා දෙක ඇතුලේ)
-  SESSION_ID: process.env.SESSION_ID || "", 
+  SESSION_ID: process.env.SESSION_ID || "Golden-Queen-MD-IhpxWTLR#ABYZ2yfEFkFZMsOSDSP4vjMKHSo3cdQLcmwAzq4O740ftrc", 
 
 //  Mongodp URl එක දාන්න 👇👇👇 (උඩු කොමා දෙක ඇතුලේ )
-  MONGODB: process.env.MONGODB || "",
+  MONGODB: process.env.MONGODB || "mongodb+srv://wwwthemiyaofficialdilan:t6kjRn2n2kXsZhry@cluster0.4jk8h.mongodb.net/",
 
 
 MAX_SIZE: 500,
